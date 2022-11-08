63f4454283ffbee0c8d8eb5ad75ea403f7be96f0
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-11-08 14:31:30 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);
+	}
+}
