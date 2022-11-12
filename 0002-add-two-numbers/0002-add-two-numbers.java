a9530bd5091d768f18a23aba99dc732076da72c4
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-11-12 20:41:35 +0900

    Time: 2 ms (99.26%), Space: 42.1 MB (94.61%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -11,30 +11,24 @@
 class Solution {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int[] array = new int[100];
-        int length = 0;
-        int idx = 0;
-        while (l1 != null) {
-            array[idx++] = l1.val;
-            l1 = l1.next;
+        ListNode dummyhead = new ListNode(0);
+        ListNode current = dummyhead;
+        int sum = 0;
+        while(l1 != null || l2 != null){
+            if (l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            sum = sum / 10;
         }
-        length = idx;
-        idx = 0;
-        while (l2 != null) {
-            array[idx] = array[idx] + l2.val;
-            l2 = l2.next;
-            idx++;
-        }
-        length = Math.max(length, idx);
-        ListNode answer = new ListNode(array[0] % 10);
-        array[1] += array[0] / 10;
-        ListNode temp = answer;
-        for (int i = 1; i < length; i++) {
-            temp.next = new ListNode(array[i] % 10);
-            array[i + 1] += array[i] / 10;
-            temp = temp.next;
-        }
-        if (array[length] != 0) temp.next = new ListNode(array[length]);
-        return answer;
+        if (sum != 0)
+            current.next = new ListNode(sum);
+        return dummyhead.next;
     }
 }
