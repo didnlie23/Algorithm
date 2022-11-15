bad514cd7278ec13bfed22da768f7bd4bc94b4bf
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-11-15 23:31:34 +0900

    Time: 0 ms (100.00%), Space: 42.5 MB (42.24%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,18 +1,16 @@
 class Solution {
 
     public int[] plusOne(int[] digits) {
-        int plus = 1;
-        Stack<Integer> stack = new Stack<>();
         for (int i = digits.length - 1; i >= 0; i--) {
-            int temp = plus + digits[i];
-            stack.push(temp % 10);
-            plus = temp / 10;
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
-        if (plus > 0) stack.push(plus);
-        int[] answer = new int[stack.size()];
-        for (int i = 0, end = stack.size(); i < end; i++) {
-            answer[i] = stack.pop();
-        }
+
+        int[] answer = new int[digits.length + 1];
+        answer[0] = 1;
         return answer;
     }
 }
