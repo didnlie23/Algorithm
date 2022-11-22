9adace69157a4b124dd34e0eb1429bd6ea2550db
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-11-22 21:43:23 +0900

    Time: 157 ms (29.66%), Space: 54 MB (63.66%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.java
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.java
@@ -1,37 +1,37 @@
 class Solution {
 
-    public int lengthOfLongestSubstringTwoDistinct(String s) {
-        LinkedList<Character> queue = new LinkedList<>();
+        public int lengthOfLongestSubstringTwoDistinct(String s) {
+        int max = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
         int start = 0;
         int end = 0;
         int length = s.length();
-        int max = 0;
-        outer:while (end < length) { // end++; 전에 길이 갱신 필수
+        while(end < length){
             char temp = s.charAt(end);
-            int size = queue.size();
-            for (int i = 0; i < size; i++) { // queue 내에 존재하는 문자였다면 길이 갱신하고 다음 문자로 이동
-                if (queue.get(i) == temp) {
-                    max = Math.max(end - start + 1, max);
+            if (map.containsKey(temp)){
+                map.put(temp, map.get(temp) + 1);
+                max = Math.max(max, end - start + 1);
+                end++;
+            }else{
+                if (map.size() < 2){
+                    map.put(temp, 1);
+                    max = Math.max(max, end - start + 1);
                     end++;
-                    if (i == 0){
-                        queue.offer(queue.poll());
-                    }
-                    continue outer;
-                }
-            }
-            if (size >= 2) {
-                char garbage = queue.poll();
-                for (int i = end - 1; i >= 0; i--) {
-                    if (garbage == s.charAt(i)) {
-                        start = i + 1;
-                        break;
+                }else{
+                    while(true){
+                        char garbage = s.charAt(start);
+                        map.put(garbage, map.get(garbage) - 1);
+                        start++;
+                        if (map.get(garbage) == 0){
+                            map.remove(garbage);
+                            break;
+                        }
                     }
                 }
             }
-            max = Math.max(end - start + 1, max);
-            queue.offer(temp);
-            end++;
         }
+
         return max;
     }
 }
