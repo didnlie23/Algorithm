85007b34284de5a030af10153db3dba1505c2295
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-11-23 11:12:10 +0900

    Time: 21 ms (10.92%), Space: 45.3 MB (27.10%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,19 +1,21 @@
 class Solution {
 
-        public boolean canAttendMeetings(int[][] intervals) {
+    public boolean canAttendMeetings(int[][] intervals) {
         int length = intervals.length;
-        if (length == 0)
-            return true;
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
+        if (length == 0) return true;
+        Arrays.sort(
+            intervals,
+            new Comparator<int[]>() {
+
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    return o1[0] - o2[0];
+                }
             }
-        });
+        );
         int end = intervals[0][1];
-        for(int i=1;i<length;i++){
-            if (intervals[i][0] < end)
-                return false;
+        for (int i = 1; i < length; i++) {
+            if (intervals[i][0] < end) return false;
             end = intervals[i][1];
         }
         return true;
