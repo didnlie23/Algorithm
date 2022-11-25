0797a8aea39f136887125a92cb6e61e8cb26091b
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-11-25 15:17:59 +0900

    Update 이 문제는 D2 입니다．.java

--- 백준/Bronze/23303. 이 문제는 D2 입니다．/이 문제는 D2 입니다．.java
+++ 백준/Bronze/23303. 이 문제는 D2 입니다．/이 문제는 D2 입니다．.java
@@ -10,7 +10,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Pattern pattern = Pattern.compile("[Dd]2", Pattern.MULTILINE);
+        Pattern pattern = Pattern.compile("[Dd]2");
         Matcher matcher = pattern.matcher(br.readLine());
         bw.write(matcher.find()?"D2":"unrated");
         bw.flush();
