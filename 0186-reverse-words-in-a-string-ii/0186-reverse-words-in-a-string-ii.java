f51c4226c503d9180549ccd860ff992bf4cc63b2
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-11-25 22:13:38 +0900

    Time: 10 ms (13.06%), Space: 55.7 MB (18.47%) - LeetHub

--- 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.java
+++ 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.java
@@ -1,42 +1,38 @@
 class Solution {
 
     public void reverseWords(char[] s) {
-        ArrayList<ArrayList<Character>> list = new ArrayList<>();
-        int length = s.length;
-        int frontIndex = 0;
-        int backIndex = 0;
-        while (frontIndex < length) {
-            if (s[frontIndex] == ' ') {
-                ArrayList<Character> temp = new ArrayList<>();
-                while (backIndex < frontIndex) {
-                    temp.add(s[backIndex]);
-                    backIndex++;
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+
+        left = 0;
+        right = 0;
+        for (int i = 0, end = s.length; i < end; i++) {
+            if (s[i] == ' ') {
+                right = i - 1;
+                while (left < right) {
+                    char temp = s[left];
+                    s[left] = s[right];
+                    s[right] = temp;
+                    left++;
+                    right--;
                 }
-                backIndex++;
-                list.add(temp);
+                left = i + 1;
             }
-            frontIndex++;
         }
-        ArrayList<Character> temp = new ArrayList<>();
-        while (backIndex < frontIndex) {
-            temp.add(s[backIndex]);
-            backIndex++;
-        }
-        list.add(temp);
-        int accum = 0;
-        for (int i = list.size() - 1; i > 0; i--) {
-            temp = list.get(i);
-            int size = temp.size();
-            for (int j = 0; j < size; j++) {
-                s[j + accum] = temp.get(j);
-            }
-            s[accum + size] = ' ';
-            accum += size + 1;
-        }
-        temp = list.get(0);
-        int size = temp.size();
-        for (int j = 0; j < size; j++) {
-            s[j + accum] = temp.get(j);
+        right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
         }
     }
 }
