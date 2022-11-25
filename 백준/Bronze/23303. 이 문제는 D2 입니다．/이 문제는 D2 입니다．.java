39a9b7fc6e3c63fa0ed8b79d87a369efbd69c126
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-11-25 15:13:17 +0900

    [Bronze III] Title: 이 문제는 D2 입니다., Time: 128 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/23303. 이 문제는 D2 입니다．/이 문제는 D2 입니다．.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Pattern pattern = Pattern.compile("[Dd]2", Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(br.readLine());
+        bw.write(matcher.find()?"D2":"unrated");
+        bw.flush();
+        bw.close();
+    }
+}
