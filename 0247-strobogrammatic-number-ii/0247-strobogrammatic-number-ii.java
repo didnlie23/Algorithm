9219d40db46f03f6a5427a8dd8813a7cfff60bc2
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-12-02 10:46:58 +0900

    Time: 74 ms (33.52%), Space: 95.5 MB (25.27%) - LeetHub

--- 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.java
+++ 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.java
@@ -12,32 +12,32 @@ class Solution {
         evenLengthStr.add("96");
         if (n == 2) return evenLengthStr;
         evenLengthStr.add("00");
-        Queue<StringBuilder> queue = new LinkedList<>();
+        Queue<String> queue = new LinkedList<>();
         List<String> result = new LinkedList<>();
 
         boolean isEven = n % 2 == 0;
 
         if (isEven) {    // 길이가 짝수일 {
             for (int i = 0, end = evenLengthStr.size(); i < end; i++) {
-                queue.offer(new StringBuilder().append(evenLengthStr.get(i)));
+                queue.offer(evenLengthStr.get(i));
             }
         } else {  // 길이가 홀수일 때
             for (int i = 0, end = oddLengthStr.size(); i < end; i++) {
-                queue.offer(new StringBuilder().append(oddLengthStr.get(i)));
+                queue.offer(oddLengthStr.get(i));
             }
         }
         int end = evenLengthStr.size() - 1;
         while (!queue.isEmpty()) {
-            StringBuilder temp = queue.poll();
+            String temp = queue.poll();
             if (temp.length() == n) {
-                result.add(temp.toString());
+                result.add(temp);
                 continue;
             }
             for (int i = 0; i < end; i++) {
-                queue.offer(new StringBuilder().append(evenLengthStr.get(i).charAt(0)).append(temp).append(evenLengthStr.get(i).charAt(1)));
+                queue.offer(evenLengthStr.get(i).charAt(0) + temp + evenLengthStr.get(i).charAt(1));
             }
             if (temp.length() + 2 != n) {
-                queue.offer(new StringBuilder().append(evenLengthStr.get(end).charAt(0)).append(temp).append(evenLengthStr.get(end).charAt(1)));
+                queue.offer(evenLengthStr.get(end).charAt(0) + temp + evenLengthStr.get(end).charAt(1));
             }
         }
 
