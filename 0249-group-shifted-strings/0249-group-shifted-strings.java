98bf8452e609dca64b813d92891776958f28afe9
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-12-03 21:15:38 +0900

    Time: 3 ms (84.70%), Space: 42.3 MB (97.15%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.java
+++ 0249-group-shifted-strings/0249-group-shifted-strings.java
@@ -4,18 +4,18 @@ class Solution {
         for(int i=0;i<strings.length;i++){
             String temp = strings[i];
             StringBuilder sb = new StringBuilder();
-            sb.append(String.valueOf(0)).append(",");
             char first = temp.charAt(0);
             for(int j=1;j<temp.length();j++){
                 int distance = temp.charAt(j) - first;
                 sb.append(String.valueOf((char)((distance+26)%26+'a')));
             }
-            if (map.containsKey(sb.toString())){
-                map.get(sb.toString()).add(temp);
+            String key = sb.toString();
+            if (map.containsKey(key)){
+                map.get(key).add(temp);
             }else{
                 ArrayList<String> list = new ArrayList<>();
                 list.add(temp);
-                map.put(sb.toString(), list);
+                map.put(key, list);
             }
         }
         List<List<String>> result = new LinkedList<>();
