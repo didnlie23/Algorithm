3a224f17df5d6e0b6c7b8e4941aae917ddffa228
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-12-03 18:21:50 +0900

    [Gold V] Title: 퇴사 2, Time: 704 ms, Memory: 304644 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -7,25 +7,17 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[N + 1];
-        int answer = 0;
         StringTokenizer st = null;
         for (int i = 0; i < N; i++) {
-            answer = Math.max(answer, dp[i]);
             st = new StringTokenizer(br.readLine());
             int t = Integer.parseInt(st.nextToken());
             int p = Integer.parseInt(st.nextToken());
-            if (i + t <= N) { // 일하는 것이 가능하다면
-                int sum = dp[i] + p;
-                if (dp[i + t] < sum) {
-                    dp[i + t] = sum;
-                }
-            }
-            if (i + 1 <= N && dp[i + 1] < dp[i]) {
-                dp[i + 1] = dp[i];
+            dp[i + 1] = Math.max(dp[i], dp[i + 1]); // 현재 날짜의 일을 안하는 경우와 다음 날의 누적 비교
+            if (i + t <= N) {   // 현재 날짜의 일을 할 수 있다면
+                dp[i + t] = Math.max(dp[i + t], dp[i] + p);
             }
         }
-        answer = Math.max(answer, dp[N]);
-        bw.write(String.valueOf(answer));
+        bw.write(String.valueOf(dp[N]));
         bw.flush();
         bw.close();
     }
