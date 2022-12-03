6b27ac4be6d18caf154e8b1a4e63ea5a3106fd0b
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-12-03 21:01:04 +0900

    Time: 3 ms (84.70%), Space: 44.1 MB (34.16%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.java
+++ 0249-group-shifted-strings/0249-group-shifted-strings.java
@@ -1,31 +1,36 @@
 class Solution {
-    public List<List<String>> groupStrings(String[] strings) {
-        Map<String, List<String>> map = new HashMap<>();
-        for(int i=0;i<strings.length;i++){
-            String temp = strings[i];
-            StringBuilder sb = new StringBuilder();
-            sb.append(String.valueOf(0)).append(",");
-            char first = temp.charAt(0);
-            for(int j=1;j<temp.length();j++){
-                sb.append(String.valueOf(calDistance(temp.charAt(j), first))).append(",");
-            }
-            if (map.containsKey(sb.toString())){
-                map.get(sb.toString()).add(temp);
-            }else{
-                ArrayList<String> list = new ArrayList<>();
-                list.add(temp);
-                map.put(sb.toString(), list);
-            }
+    // Create a hash value
+    String getHash(String s) {
+        char[] chars = s.toCharArray();
+        StringBuilder hashKey = new StringBuilder();
+        
+        for (int i = 1; i < chars.length; i++) {
+            hashKey.append((char) ((chars[i] - chars[i - 1] + 26) % 26 + 'a'));
         }
-        List<List<String>> result = new LinkedList<>();
-        for(Map.Entry<String, List<String>> entry : map.entrySet()){
-            result.add(entry.getValue());
-        }
-        return result;
+        
+        return hashKey.toString();
     }
-
-    public static int calDistance(char c1, char c2){
-        int distance = c1 - c2;
-        return distance >= 0?distance:distance+26;
+    
+    public List<List<String>> groupStrings(String[] strings) {
+        Map<String, List<String>> mapHashToList = new HashMap<>();
+        
+        // Create a hash_value (hashKey) for each string and append the string
+        // to the list of hash values i.e. mapHashToList["cd"] = ["acf", "gil", "xzc"]
+        for (String str : strings ) {
+            String hashKey = getHash(str);
+            if (mapHashToList.get(hashKey) == null) {
+                mapHashToList.put(hashKey, new ArrayList<>());
+            } 
+            mapHashToList.get(hashKey).add(str);
+        }
+        
+        // Iterate over the map, and add the values to groups
+        List<List<String>> groups = new ArrayList<>();
+        for (List<String> group : mapHashToList.values()) {
+            groups.add(group);
+        }
+        
+        // Return a list of all of the grouped strings
+        return groups;
     }
 }
