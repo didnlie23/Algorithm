07b9bade424621bde992adac44116f3635c4f62c
didnlie23 <74448635+didnlie23@users.noreply.github.com>
2022-12-03 23:29:53 +0900

    Time: 2 ms (84.09%), Space: 52.7 MB (31.55%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -5,15 +5,11 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             right += nums[i];
         }
-        right -= nums[0];
-        for(int i=1;i<nums.length;i++){
-            if (left == right)
-                return i - 1;
-            left += nums[i-1];
-            right -= nums[i];
+        for(int i=0;i<nums.length;i++){
+            if (left == right - left - nums[i])
+                return i;
+            left += nums[i];
         }
-        if (left == right)
-            return nums.length -1;
         return -1;
     }
 }
